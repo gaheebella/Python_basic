184ea82fff84d6d74b2cf62a26dcea8f46189839
Gahee Yang <gahee9006@gmail.com>
2024-05-27 12:35:31 +0900

    정규식을 이용하여 입력한 문자열에서 휴대폰 번호 찾기

--- lec9_문자열/정규식_핸드폰번호 찾기.py
+++ lec9_문자열/정규식_핸드폰번호 찾기.py
@@ -1,7 +1,9 @@
-import re
+import re #정규식 함수 모듈
 
 phone_num = input()
 pattern = r'010-\d\d\d\d-\d\d\d\d' #정규식
+# r:원시문자열_백슬래시를 이스케이프문자가 아닌 문자열의 일부로 취급
 
 found = re.search(pattern, phone_num)
 print("발견된 핸드폰 번호: " + found.group())
+#정규식 패턴 발견시 match객체의 group()호출-> 일치된 문자열 반환
