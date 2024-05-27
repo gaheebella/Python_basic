ea40dbaf8928460aabe22eaeaa53a6331c654399
Gahee Yang <gahee9006@gmail.com>
2024-05-27 13:53:55 +0900

    문장의 단어 등장횟수 검사

--- lec9_문자열/정규식_단어 카운터.py
+++ lec9_문자열/정규식_단어 카운터.py
@@ -6,7 +6,7 @@ for word in text_data.split(): #text_data를 공백기준으로 잘라서 한 �
     if word in word_dic: #딕셔너리 안에 word가 이미 있으면
         word_dic[word] += 1 #해당 word(key값)의 count(value값)를 1 증가
     else:
-        word_dic[word] = 1 #없으면 1로 초기화
+        word_dic[word] = 1 #없으면 1로 초기화시키기
 
 for word,count in sorted(word_dic.items()): #key,value값 출력_딕셔너리는 items이용
     print(word, "의 등장횟수: ", count)
