2a7774be31c2a1fa8afe584aceca7cdda5f14cf6
Gahee Yang <gahee9006@gmail.com>
2024-06-03 11:10:09 +0900

    특수메소드

--- lec12_객체와 클래스/Circle 클래스.py
+++ lec12_객체와 클래스/Circle 클래스.py
@@ -10,6 +10,14 @@ class Circle:
     def getPerimeter(self):
         return 2 * math.pi * self.radius
 
-c = Circle(10)
-print("원의 면적 = ", c.getArea())
-print("원의 둘레 = ", c.getPerimeter())
+    def __eq__(self, other):
+        return self.radius == other.radius  # __eq__()메소드가 정의된 객체는 ==연산자를 이용해 비교가능
+
+c1 = Circle(10)
+c2 = Circle(10)
+
+if c1 == c2:
+    print("c1과 c2의 원의 반지름이 동일함")
+
+print("c1원의 면적 = ", c1.getArea())
+print("c2원의 둘레 = ", c2.getPerimeter())
